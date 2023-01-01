82894c71e293141e759f5d48293eaa1d03a2190b
mfoot123 <mfoot456@gmail.com>
2022-12-31 20:50:26 -0800

    First Commit

---
+++ client/src/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AUTH_URL = 
+// client id = public so we can put on our front end
+// scopes give us access to certain permissions from the user
+"https://accounts.spotfiy.com/authorize?client_id=d4fb8ac6b7174feda68bce832fd7777f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+    return(
+        <div>
+
+        </div>
+    )
+}
