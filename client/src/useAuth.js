09a310b436f86df5ed1d6209f199d053a74afd45
mfoot123 <mfoot456@gmail.com>
2023-01-01 02:07:31 -0800

    auto refresh v1

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -10,8 +10,38 @@ export default function useAuth(code) {
         axios.post('http://localhost:3001/login', {
             code,
         }).then(res => {
-            console.log(res.data)
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            // removes the code from the end of our url
+            window.history.pushState({}, null, '/')
+        }).catch(() => {
+            // send back to local host if error is thrown
+            window.location = "/"
         })
         // run use effect every time our code changes
     }, [code])
+
+    useEffect(() => {
+        if(!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+        axios
+        .post('http://localhost:3001/refresh', {
+            refreshToken,
+        }).then(res => {
+            // some thing as above but only for expires in and access token
+            setAccessToken(res.data.accessToken)
+            setExpiresIn(res.data.expiresIn)
+        }).catch(() => {
+            // send back to local host if error is thrown
+            window.location = "/"
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+        // whenever our refresh token or expires in changes
+        // run use effect
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -8,24 +8,49 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
+// create a rout to refresh our token
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        // hard code our necessary data
+        redirectUri: 'http://localhost:3000',
+        clientId: 'd4fb8ac6b7174feda68bce832fd7777f',
+        clientSecret: '7382f3338448430b97b8a1ce85f1fc2b',
+        refreshToken,
+    })
+
+    spotifyApi.refreshAccessToken()
+    .then(data => {
+            //console.log('The access token has been refreshed!');
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+        }).catch(() => {
+            res.sendStatus(400)
+    }) //console.log('Could not refresh access token', err);
+})
+
 app.post('/login', (req,res) => {
     const code = req.body.code
-    const spotifyApu = new SpotifyWebApi({
+    const spotifyApi = new SpotifyWebApi({
         // hard code our necessary data
         redirectUri: 'http://localhost:3000',
         clientId: 'd4fb8ac6b7174feda68bce832fd7777f',
         clientSecret: '7382f3338448430b97b8a1ce85f1fc2b',
     })
 
-    spotifyWebApi.authorizationCodeGrant(code).then(data => {
+    spotifyApi.authorizationCodeGrant(code).then(data => {
         res.json({
             // get our access and refresh tokens
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         })
-    }).catch(() => {
+    })
+    .catch(err => {
         // send errors if they are thrown
+        console.log(err)
         res.sendStatus(400)
     })
 })
